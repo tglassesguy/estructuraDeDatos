1fe59b91fcdc55ab1c32df8f324a8e2da938eaea
DepredadorZ <2220161014@estudiantesunibague.edu.co>
2018-04-06 13:05:41 -0500

    Se hizo la clase DAO

--- Taller2/src/mundo/dao/LibroDAO.java
+++ Taller2/src/mundo/dao/LibroDAO.java
@@ -1,5 +1,6 @@
 package mundo.dao;
 
+import java.sql.ResultSet;
 import java.util.Collection;
 
 import mundo.db.Conexion;
@@ -7,31 +8,28 @@ import mundo.dto.LibroDTO;
 
 public class LibroDAO implements IDao{
 	
-	LibroDTO libroDto;
 	
-	public LibroDAO()
-	{
-	   libroDto = new LibroDTO();
-	}
+
 
 	@Override
 	public void insertar(Conexion con, LibroDTO libro) {
 		// TODO Auto-generated method stub
-		
+		con.ejecutaActualizacion(libro.insertar());
 	}
 
 	@Override
 	public void eliminar(Conexion con, LibroDTO libro) {
 		// TODO Auto-generated method stub
-		
+		con.ejecutaActualizacion(libro.eliminar());
 	}
 
 	@Override
 	public void modificar(Conexion con, LibroDTO libro) {
 		// TODO Auto-generated method stub
-		
+		con.ejecutaActualizacion(libro.modificar());
 	}
 
+	//NO SE PUEDE HACER COLLECTION SI NO GENERICIDAD Y PATRON DE DISEÑO REFACTORY
 	@Override
 	public Collection<LibroDTO> consultarLibros(Conexion con) {
 		// TODO Auto-generated method stub
@@ -41,6 +39,28 @@ public class LibroDAO implements IDao{
 	@Override
 	public LibroDTO consultarLibrosPorId(Conexion con, LibroDTO libro) {
 		// TODO Auto-generated method stub
+		ResultSet rs = con.ejecutaConsulta(libro.consultarTodos());
+		
+		try {
+			
+			while(rs.next())
+			{
+				String[] fecha = rs.getString(4).split("/");
+				
+				libro.setNombre(rs.getString(2));
+				libro.setAutor(rs.getString(3));
+				libro.setISBN(rs.getString(3));
+				libro.setFecha(Integer.parseInt(fecha[0]));
+				libro.setMes(Integer.parseInt(fecha[1]));
+				libro.setAño(Integer.parseInt(fecha[2]));
+			}
+			
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		return libro;
 		return null;
 	}
 	
