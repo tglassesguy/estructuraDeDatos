ac6c4392469c191f81f6dc14ffc88fd772a6771b
Javier Collazos <jcollazosgomez@gmail.com>
2018-04-08 22:39:57 -0500

    Se ha eliminado los packages dao, dto y facade. El socket recibe Strings.

--- Taller2/src/mundo/server/Servidor.java
+++ Taller2/src/mundo/server/Servidor.java
@@ -1,5 +1,103 @@
 package mundo.server;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+import mundo.db.Servicio;
+
 public class Servidor {
+
+	public static void main(String[] args) throws IOException {
+		
+		Servicio ser = new Servicio();
+		
+		Socket so;
+        DataOutputStream salida;
+        String mensajeRecibido;
+        String respuesta = "";
+        BufferedReader entrada;
+
+        ServerSocket servidor = new ServerSocket(12345);
+
+        so = new Socket();
+
+        System.out.println("Esperando conexion con el cliente.");
+
+
+        so = servidor.accept();
+        System.out.println("El cliente se ha conectado.");
+        
+        salida = new DataOutputStream(so.getOutputStream());
+        entrada = new BufferedReader( new InputStreamReader(so.getInputStream()));
+        
+        mensajeRecibido = entrada.readLine();
+        
+        mensajeRecibido = mensajeRecibido.trim().toUpperCase();
+        
+        if(mensajeRecibido.contains("FIN"))
+        {
+        	respuesta= "Se cerrará el programa.";
+        }
+        else if(mensajeRecibido.contains("INSERTAR"))
+        {
+        	try {
+        		
+        		ser.getCon().ejecutaActualizacion(mensajeRecibido);
+        		
+        		respuesta= "Se ha insertado el elemento.";
+        		
+        	}
+        	catch(Exception e)
+        	{
+        		e.printStackTrace();
+        	}
+        }
+        else if(mensajeRecibido.contains("ELIMINAR"))
+        {
+        	try {
+        		
+        		ser.getCon().ejecutaActualizacion(mensajeRecibido);
+        		
+        		respuesta= "Se ha eliminado el elemento.";
+        	}
+        	catch(Exception e)
+        	{
+        		e.printStackTrace();
+        	}
+        }
+        else if(mensajeRecibido.contains("UPDATE")) 
+        {
+        	try {
+        		
+        		ser.getCon().ejecutaActualizacion(mensajeRecibido);
+        		
+        		respuesta= "Se ha actualizado el elemento.";
+        		
+        	}
+        	catch(Exception e)
+        	{
+        		e.printStackTrace();
+        	}
+        }
+        else
+        {
+        	respuesta= "La operación no correspone a un comando SQL.";
+        }
+        
+        JOptionPane.showMessageDialog(null, respuesta);
+        
+        salida.close();
+        entrada.close();
+        so.close();
+        servidor.close();
+       
+	}
 
 }
--- Taller2/src/mundo/test/Cliente.java
+++ Taller2/src/mundo/test/Cliente.java
@@ -1,19 +1,38 @@
 package mundo.test;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
 import mundo.db.Servicio;
 
 public class Cliente {
 	
-	public Cliente() {
-		
-		Servicio ser= new Servicio();
-		
-		
-		
-		
-	}
+	public final static int PUERTO = 12345;
 	
-	public static void main(String[] args) {
-		new Cliente();
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		DataOutputStream salida;
+		//DataInputStream entrada;
+		
+		Socket cliente = new Socket("localhost", PUERTO);
+		
+		System.out.println("Cliente conectado con el servidor.");
+		
+		salida = new DataOutputStream(cliente.getOutputStream());
+		//entrada = new DataInputStream(cliente.getInputStream());
+		
+		String data = JOptionPane.showInputDialog("Inserte el número de la operación que desea realizar, FIN para salir");
+		
+		salida.writeUTF(data);
+		salida.flush();
+		
+		salida.close();
+		cliente.close();
+		
 	}
 }
