bc5407c2fa8eb8ba7a0e6894cdec38bc635aa952
Javier Collazos <jcollazosgomez@gmail.com>
2018-04-16 16:55:42 -0500

    Clases base del proyecto

---
+++ Blume/src/mundo/test/Test.java
@@ -0,0 +1,17 @@
+package mundo.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	
+	public Test()
+	{
+		
+		
+	}
+	public static void main(String[] args) {
+		new Test();
+	}
+
+}
