c981312e85c7538b02722036559492eed5b8950b
Renbrack <felipealejandrodl@gmail.com>
2018-04-18 17:28:19 -0500

    Socket

---
+++ Blume/src/mundo/server/Usuario.java
@@ -0,0 +1,19 @@
+package mundo.server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+
+public class Usuario {
+	
+	public final static int PUERTO = 9999;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DataOutputStream out;
+		BufferedReader in;
+		
+
+	}
+
+}
