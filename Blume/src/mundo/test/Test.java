0dfc5d1f8637c018c84867b1357ae2338267a68b
DepredadorZ <2220161014@estudiantesunibague.edu.co>
2018-04-19 11:39:42 -0500

    Se realizo algo del test :v

--- Blume/src/mundo/test/Test.java
+++ Blume/src/mundo/test/Test.java
@@ -3,12 +3,23 @@ package mundo.test;
 import java.util.ArrayList;
 import java.util.List;
 
+import mundo.db.Servicio;
+import mundo.facade.Facade;
+
 public class Test {
-	
+
+	Servicio servicio;
+	Facade facade;
 	public Test()
 	{
+		servicio = new Servicio();
+		facade = new Facade();
 		
 		
+		
+		
+		Mensaje mensaje = new Mensaje("INSERT INTO ARTICULOS (ID_ARTICULO, TITULO, AUTOR, FECHA_PUBLICACION)VALUES (2,'Javier','Felipe el crack','18/02/1998')");
+		facade.insertar(servicio.getCon(), mensaje);	
 	}
 	public static void main(String[] args) {
 		new Test();
