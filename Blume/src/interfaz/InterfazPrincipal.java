51634e72f82d742dc3c245f42c1d5f434819ba20
Javier Collazos <jcollazosgomez@gmail.com>
2018-04-26 08:33:31 -0500

    Pequeñas modificaciones en la interfaz

--- Blume/src/interfaz/InterfazPrincipal.java
+++ Blume/src/interfaz/InterfazPrincipal.java
@@ -2,6 +2,7 @@ package interfaz;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
@@ -9,23 +10,23 @@ public class InterfazPrincipal extends JFrame {
 	
 	private PanelTitulo panelTitulo;
 	private PanelSeleccion panelSeleccion;
-	private PanelMostrar panelMostrar;
+	//private PanelMostrar panelMostrar;
 	
 	private static final long serialVersionUID = 1L;
 	
 	public InterfazPrincipal()
 	{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    setLocation((Toolkit.getDefaultToolkit().getScreenSize().width  - getSize().width) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - getSize().height) / 2);
 		setSize(new Dimension(600, 600));
 		setResizable(false);
-		setTitle("Blume");
+		setTitle("Blume | DataBase Control System");
 		
 		setLayout(new BorderLayout());
 		
 		panelTitulo = new PanelTitulo();
 		panelSeleccion = new PanelSeleccion(this);
-		panelMostrar =  new PanelMostrar();
-		
+		//panelMostrar =  new PanelMostrar();
 		
 		add(panelTitulo,BorderLayout.NORTH);
 		add(panelSeleccion, BorderLayout.CENTER);
